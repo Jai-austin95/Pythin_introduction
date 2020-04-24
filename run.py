52140733e2478636ccd294581abee6da0a9b33a0
Jai-austin95 <jai.austin95@gmail.com>
2020-04-24 16:59:06 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,23 +1,43 @@
 import os
-from flask import Flask
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session
+
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
+messages = []
 
 
-@app.route("/")
+def add_messages(username, message):
+    """Add messages to the `messages` list"""
+    now = datetime.now().strftime("%H:%M:%S")
+    messages_dict = {"timestamp": now, "from": username, "message": message}
+    messages.append(messages_dict)
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     """Main page with instructions"""
-    return "To send a message use: /USERNAME/MESSAGE"
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+
+    if "username" in session:
+        return redirect(session["username"])
+
+    return render_template("index.html")
 
 
 @app.route("/<username>")
 def user(username):
-    return "Hi " + username
+    """Display chat messages"""
+    return "<h1>Welcome, {0}</h1>{1}".format(username, messages)
 
 
 @app.route("/<username>/<message>")
 def send_message(username, message):
-    return "{0}: {1}".format(username, message)
+    """Create a new message and redirect back to the chat page"""
+    add_messages(username, message)
+    return redirect("/" + username)
 
 
 app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
